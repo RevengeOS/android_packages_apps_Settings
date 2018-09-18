c96df2a5ddd6c035ad6092001a6f8b4f6aa77bc0
Lucchetto <lucchetto.tie@live.com>
2018-09-18 22:29:08 +0200

    Revert "SoundSettings: Add screenshot sounds settings"
    This reverts commit 16ad61607f6d1fbf55ec4ad938dac43225ab2dd1.

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -265,8 +265,6 @@ public class SoundSettings extends DashboardFragment {
                 new BootSoundPreferenceController(context);
         final EmergencyTonePreferenceController emergencyTonePreferenceController =
                 new EmergencyTonePreferenceController(context, fragment, lifecycle);
-        final ScreenshotSoundPreferenceController screenshotSoundPreferenceController =
-                new ScreenshotSoundPreferenceController(context, fragment, lifecycle);
 
         controllers.add(dialPadTonePreferenceController);
         controllers.add(screenLockSoundPreferenceController);
@@ -278,7 +276,6 @@ public class SoundSettings extends DashboardFragment {
         controllers.add(dockAudioMediaPreferenceController);
         controllers.add(bootSoundPreferenceController);
         controllers.add(emergencyTonePreferenceController);
-        controllers.add(screenshotSoundPreferenceController);
         controllers.add(new PreferenceCategoryController(context,
                 "other_sounds_and_vibrations_category").setChildren(
                 Arrays.asList(dialPadTonePreferenceController,

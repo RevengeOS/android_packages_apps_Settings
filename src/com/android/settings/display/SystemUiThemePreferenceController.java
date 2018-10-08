86ac213ea82566181a534af915001faefaa0c53c
Shubham Singh <coolsks94@gmail.com>
2018-10-08 20:13:12 +0530

    Restart SystemUI after theme change
    Change-Id: Ic7737430a9afb8a16bc8471ce3c6a2b6280acb06
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- src/com/android/settings/display/SystemUiThemePreferenceController.java
+++ src/com/android/settings/display/SystemUiThemePreferenceController.java
@@ -28,6 +28,7 @@ import android.util.FeatureFlagUtils;
 import com.android.settings.core.BasePreferenceController;
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.internal.util.aospextended.AEXUtils;
 
 /**
  * Setting where user can pick if SystemUI will be light, dark or try to match
@@ -61,6 +62,7 @@ public class SystemUiThemePreferenceController extends BasePreferenceController
         int value = Integer.parseInt((String) newValue);
         Settings.Secure.putInt(mContext.getContentResolver(), THEME_MODE, value);
         refreshSummary(preference);
+        AEXUtils.restartSystemUi(mContext);
         return true;
     }
 

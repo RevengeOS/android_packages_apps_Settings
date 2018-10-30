AospExtended/platform_packages_apps_Settings

Build Extensions into Settings   * Add it on dashboard   * Set proper priority to show it below Apps & Notifications
Signed-off-by: Shubham Singh <coolsks94@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -127,6 +127,8 @@ import com.android.settings.wifi.WifiInfo;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
+import org.aospextended.extensions.PartsFragment;
+
 public class SettingsGateway {
 
     /**
@@ -244,6 +246,7 @@ public class SettingsGateway {
             EnterprisePrivacySettings.class.getName(),
             WebViewAppPicker.class.getName(),
             LockscreenDashboardFragment.class.getName(),
+            PartsFragment.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -281,5 +284,6 @@ public class SettingsGateway {
             Settings.DateTimeSettingsActivity.class.getName(),
             Settings.DeviceInfoSettingsActivity.class.getName(),
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
+            Settings.ExtensionsSettingsActivity.class.getName(),
     };
 }

src/com/android/settings/dashboard/DashboardFragmentRegistry.java
@@ -35,6 +35,8 @@ import com.android.settings.notification.SoundSettings;
 import com.android.settings.system.SystemDashboardFragment;
 import com.android.settingslib.drawer.CategoryKey;
 
+import org.aospextended.extensions.PartsFragment;
+
 import java.util.Map;
 
 /**
@@ -86,6 +88,8 @@ public class DashboardFragmentRegistry {
                 CategoryKey.CATEGORY_SYSTEM_DEVELOPMENT);
         PARENT_TO_CATEGORY_KEY_MAP.put(ConfigureNotificationSettings.class.getName(),
                 CategoryKey.CATEGORY_NOTIFICATIONS);
+        PARENT_TO_CATEGORY_KEY_MAP.put(PartsFragment.class.getName(),
+                CategoryKey.CATEGORY_SYSTEM_DEVELOPMENT);
 
         CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap<>(PARENT_TO_CATEGORY_KEY_MAP.size());
 

RevengeOS/android_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -137,6 +137,8 @@ import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
+import org.aospextended.extensions.Extensions;
+
 public class SettingsGateway {
 
     /**
@@ -261,6 +263,7 @@ public class SettingsGateway {
             DirectoryAccessDetails.class.getName(),
             ToggleBackupSettingFragment.class.getName(),
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
+            Extensions.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -299,5 +302,6 @@ public class SettingsGateway {
             Settings.DeviceInfoSettingsActivity.class.getName(),
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
             Settings.MyDeviceInfoActivity.class.getName(),
+            Settings.ExtensionsSettingsActivity.class.getName(),
     };
 }

src/com/android/settings/dashboard/DashboardFragmentRegistry.java
@@ -40,6 +40,8 @@ import com.android.settings.security.SecuritySettings;
 import com.android.settings.system.SystemDashboardFragment;
 import com.android.settingslib.drawer.CategoryKey;
 
+import org.aospextended.extensions.Extensions;
+
 import java.util.Map;
 
 /**
@@ -101,6 +103,8 @@ public class DashboardFragmentRegistry {
             CategoryKey.CATEGORY_GESTURES);
         PARENT_TO_CATEGORY_KEY_MAP.put(NightDisplaySettings.class.getName(),
             CategoryKey.CATEGORY_NIGHT_DISPLAY);
+        PARENT_TO_CATEGORY_KEY_MAP.put(Extensions.class.getName(),
+                CategoryKey.CATEGORY_SYSTEM_DEVELOPMENT);
 
         CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap<>(PARENT_TO_CATEGORY_KEY_MAP.size());
 
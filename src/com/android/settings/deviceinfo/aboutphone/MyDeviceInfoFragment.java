d3d6c8c51b90e8804015a6f748e794ac2bd5d95c
Stefan Berger <s.berger81@gmail.com>
2019-02-01 22:04:37 +0100

    Settings: Bring back Uptime
    Add UpTimePreferenceController in "About Phone"
    
    Change-Id: I00c362f0440746bca67f2cf10cd8e8f49385ddfd

--- src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
+++ src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -50,6 +50,7 @@ import com.android.settings.deviceinfo.WifiMacAddressPreferenceController;
 import com.android.settings.deviceinfo.firmwareversion.FirmwareVersionPreferenceController;
 import com.android.settings.deviceinfo.imei.ImeiInfoPreferenceController;
 import com.android.settings.deviceinfo.SELinuxStatusPreferenceController;
+import com.android.settings.deviceinfo.UpTimePreferenceController;
 import com.android.settings.deviceinfo.simstatus.SimStatusPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.widget.EntityHeaderController;
@@ -131,6 +132,7 @@ public class MyDeviceInfoFragment extends DashboardFragment
         controllers.add(new SELinuxStatusPreferenceController(context));
         controllers.add(
                 new BuildNumberPreferenceController(context, activity, fragment, lifecycle));
+        controllers.add(new UpTimePreferenceController(context));
         return controllers;
     }
 

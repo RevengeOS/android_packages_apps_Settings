fb0f8b2e93737818739ce10a6acfa0d52da0affb
Lucchetto <lucchetto.tie@live.com>
2019-10-06 22:31:17 +0200

    Revert "Allow navigation gesture bar to be hidden [2/2]"
    This reverts commit df69a42dc2543575a076751df506f0ab865e4252.
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- src/com/android/settings/gestures/GestureNavigationBackSensitivityDialog.java
+++ src/com/android/settings/gestures/GestureNavigationBackSensitivityDialog.java
@@ -16,25 +16,18 @@
 
 package com.android.settings.gestures;
 
-import static android.os.UserHandle.USER_CURRENT;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.settings.SettingsEnums;
 import android.content.Context;
 import android.content.om.IOverlayManager;
-import android.content.om.OverlayInfo;
 import android.os.Bundle;
-import android.os.RemoteException;
 import android.os.ServiceManager;
-import android.provider.Settings;
 import android.view.View;
 import android.widget.SeekBar;
-import android.widget.Switch;
 
 import com.android.settings.R;
 import com.android.settings.core.instrumentation.InstrumentedDialogFragment;
-
-import com.revengeos.internal.util.NavBarUtils;
 
 /**
  * Dialog to set the back gesture's sensitivity in Gesture navigation mode.
@@ -64,21 +57,10 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        final IOverlayManager overlayManager = IOverlayManager.Stub.asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
         final View view = getActivity().getLayoutInflater().inflate(
                 R.layout.dialog_back_gesture_sensitivity, null);
         final SeekBar seekBar = view.findViewById(R.id.back_sensitivity_seekbar);
         seekBar.setProgress(getArguments().getInt(KEY_BACK_SENSITIVITY));
-        Switch mNavBarGesturalHideNav = view.findViewById(R.id.nav_bar_gestural_hide_nav_switch);
-        boolean mIsGesturalNavBarHidden = NavBarUtils.isGesturalNavBarHidden(getContext(), USER_CURRENT);
-        if (SystemNavigationPreferenceController.isEdgeToEdgeEnabled(getContext())) {
-            OverlayInfo ovInfo = null;
-            try {
-                ovInfo = overlayManager.getOverlayInfo(NavBarUtils.NAV_BAR_GESTURAL_HIDE_NAV_OVERLAY, USER_CURRENT);
-            } catch (RemoteException e) { }
-            mIsGesturalNavBarHidden = mIsGesturalNavBarHidden && ovInfo != null && (ovInfo.state == OverlayInfo.STATE_ENABLED);
-        }
-        mNavBarGesturalHideNav.setChecked(NavBarUtils.isGesturalNavBarHidden(getContext(), USER_CURRENT));
         return new AlertDialog.Builder(getContext())
                 .setTitle(R.string.back_sensitivity_dialog_title)
                 .setMessage(R.string.back_sensitivity_dialog_message)
@@ -87,15 +69,12 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
                     int sensitivity = seekBar.getProgress();
                     getArguments().putInt(KEY_BACK_SENSITIVITY, sensitivity);
                     SystemNavigationGestureSettings.setBackSensitivity(getActivity(),
-                            overlayManager, sensitivity);
-                    final boolean mNavBarGesturalHideNavEnabled = mNavBarGesturalHideNav.isChecked();
-                    if (SystemNavigationPreferenceController.isEdgeToEdgeEnabled(getContext())) {
-                        if (NavBarUtils.setGesturalNavBarHiddenOverlay(overlayManager, USER_CURRENT, mNavBarGesturalHideNavEnabled)) {
-                            Settings.System.putIntForUser(getContext().getContentResolver(),
-                                    Settings.System.NAV_BAR_GESTURAL_HIDE_NAV, mNavBarGesturalHideNavEnabled ? 1 : 0, USER_CURRENT);
-                        }
-                    }
+                            getOverlayManager(), sensitivity);
                 })
                 .create();
+    }
+
+    private IOverlayManager getOverlayManager() {
+        return IOverlayManager.Stub.asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
     }
 }
